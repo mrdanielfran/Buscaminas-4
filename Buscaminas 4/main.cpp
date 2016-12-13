b0ce8820c48a6eb676a1b6fd2e3048b948ebdb41
Francisco Daniel <Daniel@macbook-pro.local>
2016-12-13 13:27:12 -0600

    Initial Commit

---
+++ Buscaminas 4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Buscaminas 4
+//
+//  Created by Daniel on 13/12/16.
+//  Copyright © 2016 daniel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
